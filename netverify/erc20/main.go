ca82dca8c1b65482c461b516920c2023d3601ab2
Matt Wang <mattwangming@gmail.com>
2019-07-26 16:23:23 +0800

    erc20 txs list added on 8849

---
+++ netverify/erc20/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/WebInJumio/netverify"
+
+func main() {
+	netverify.Ercscantxlist()
+}
--- netverify/etherscan.go
+++ netverify/etherscan.go
@@ -14,6 +14,13 @@ type Ethinput struct {
 	Offset string `form:"offset"`
 }
 
+type Ercinput struct {
+	Contract string `form:"contract"`
+	Addr     string `form:"addr"`
+	Page     string `form:"page"`
+	Offset   string `form:"offset"`
+}
+
 //Etherscantxlist
 func Etherscantxlist() {
 	r := gin.Default()
@@ -28,6 +35,20 @@ func Etherscantxlist() {
 	r.Run(":8848")
 }
 
+//Ercscantxlist
+func Ercscantxlist() {
+	r := gin.Default()
+	r.GET("/txlist", func(c *gin.Context) {
+		var ercinput Ercinput
+		if c.ShouldBind(&ercinput) == nil {
+			resp := Ercscan(ercinput.Contract, ercinput.Addr, ercinput.Page, ercinput.Offset)
+			c.JSON(200, resp)
+		}
+
+	})
+	r.Run(":8849")
+}
+
 //Ethscan
 func Ethscan(addr, page, offset string) string {
 	baseurl := "https://api.etherscan.io/api?module=account&action=txlist"
@@ -36,6 +57,31 @@ func Ethscan(addr, page, offset string) string {
 	os := offset
 
 	url := baseurl + "&address=" + ad + "&startblock=0&endblock=99999999" + "&page=" + pa + "&offset=" + os + "&sort=asc&apikey=YourApiKeyToken"
+	// req, _ := http.NewRequest("GET", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(respBody)
+
+}
+
+//Ercscan
+func Ercscan(contract, addr, page, offset string) string {
+	baseurl := "https://api.etherscan.io/api?module=account&action=tokentx"
+	ca := contract
+	ad := addr
+	pa := page
+	os := offset
+
+	url := baseurl + "&contractaddress=" + ca + "&address=" + ad + "&page=" + pa + "&offset=" + os + "&sort=asc&apikey=YourApiKeyToken"
 	// req, _ := http.NewRequest("GET", url)
 	resp, err := http.Get(url)
 	if err != nil {
