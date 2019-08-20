3d199fc3e5645d840e8895bd353f52325f1f4890
Matt Wang <mattwangming@gmail.com>
2019-08-20 15:50:26 +0800

    small adjustment

--- netverify/quickscan.go
+++ netverify/quickscan.go
@@ -10,16 +10,15 @@ func Quickscan() {
 		//example for input pattern:"cosmos:<address>?contractAddress=uatom&decimal=<decimal>&value=<value>"
 		data := `[
 					{
-						"chain":"^ethereum:$",
 						"address":"^0x[0-9a-fA-F]{40}$",
+						"comment":"This is for normal ethereum template checking"
+					},			
+					{
+						"address":"^ethereum:0x[0-9a-fA-F]{40}$",
 						"contractAddress":"^contractAddress=0x[0-9a-fA-F]{40}$",
 						"decimal":"^decimal=[0-18]$",
 						"value":"^value=[1-9]\d*\.\d*|0\.\d*[1-9]\d*|0$",	
 						"comment":"This is for imToken ethereum template checking"	
-					},
-					{
-						"address":"^0x[0-9a-fA-F]{40}$",
-						"comment":"This is for other ethereum template checking"
 					}
 				]`
 		c.String(200, data)
@@ -27,19 +26,18 @@ func Quickscan() {
 	r.GET("/QRdata/cosmos", func(c *gin.Context) {
 		//example for input pattern:"cosmos:<address>?contractAddress=uatom&decimal=<decimal>&value=<value>"
 		data := `[
-					{	
-						"chain":"^cosmos:$",
+					{
 						"address":"^cosmos1[0-9a-z]{38}$",
+						"comment":"This is for normal cosmos networks template checking"
+					},
+					{	
+						"address":"^cosmos:cosmos1[0-9a-z]{38}$",
 						"contractAddress":"^uatom$",
 						"decimal":"^decimal=[0-18]$",
 						"value":"^value=[1-9]\d*\.\d*|0\.\d*[1-9]\d*|0$",
 						"comment":"This is for imtoken cosmos networks template checking"
-					},
-					{
-						"address":"^cosmos1[0-9a-z]{38}$",
-						"comment":"This is for other cosmos networks template checking"
 					}
-					]`
+				]`
 		c.String(200, data)
 	})
 
