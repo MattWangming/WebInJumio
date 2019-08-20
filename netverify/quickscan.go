e8788995a8b23d04eee5a3f63373b52eb681c87f
Matt Wang <mattwangming@gmail.com>
2019-08-20 13:54:28 +0800

    complile regix

--- netverify/quickscan.go
+++ netverify/quickscan.go
@@ -11,13 +11,27 @@ func Quickscan() {
 		// 	c.JSON(400, gin.H{"msg": err})
 		// 	return
 		// }
+		//example for input pattern:"cosmos:<address>?contractAddress=uatom&decimal=<decimal>&value=<value>"
 		data := `{
-			"ethereum": "<address>",
-			"cosmos": "<address>",
-			"<address>",
-			"contractAddress",
-			"decimal",
-			"value"
+			"1" : {
+				"ethereum":{
+					"address":"^0x[0-9a-fA-F]{40}\?$",
+					"contractAddress":"^0x[0-9a-fA-F]{40}\&$",
+					"decimal":"^[0-18]\&$",
+					"value":"^[1-9]\d*\.\d*|0\.\d*[1-9]\d*|0$"
+				},
+				"cosmos":{
+					"address":"^cosmos1[0-9a-z]{38}\?$",
+					"contractAddress":"^uatom\&$",
+					"decimal":"^[0-18]\&$",
+					"value":"^[1-9]\d*\.\d*|0\.\d*[1-9]\d*|0$"
+				}
+			}
+			"2" : {
+				"EthAddress": "^0x[0-9a-fA-F]{40}$",
+				"CosmosAddress": "^cosmos1[0-9a-z]{38}$",
+				"QosAddress": "address1[0-9a-z]{38}$"
+			}
 		}`
 		c.String(200, data)
 
